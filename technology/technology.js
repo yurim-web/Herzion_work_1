77c58ac44aac08d03169d0eb0fc88cadd3909200
lyl5152 <lyl5152@cmcm.co.kr>
2025-05-08 17:35:20 +0900

    250508_1

--- technology/technology.js
+++ technology/technology.js
@@ -5,8 +5,11 @@ const researchSwiper = new Swiper(".researchSwiper", {
   centeredSlides: false,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
-    type: "progressbar",
+    type: "custom",
+    renderCustom: function (swiper, current, total) {
+      // 아무것도 반환하지 않음(직접 조작)
+      return "";
+    },
   },
   breakpoints: {
     320: {
@@ -22,7 +25,19 @@ const researchSwiper = new Swiper(".researchSwiper", {
       spaceBetween: 40,
     },
   },
+  on: {
+    slideChange: function () {
+      const bar = document.querySelector(".custom-progressbar-bar");
+      const total = this.slides.length - this.params.slidesPerView + 1;
+      const percent = 100 / total;
+      bar.style.width = percent + "%";
+      bar.style.left = percent * this.activeIndex + "%";
+    },
+  },
 });
+
+// 초기 위치 세팅
+researchSwiper.emit("slideChange");
 
 // 모바일 Thesis 스와이퍼 초기화
 const mobileThesisSwiper = new Swiper(".mobileThesisSwiper", {
